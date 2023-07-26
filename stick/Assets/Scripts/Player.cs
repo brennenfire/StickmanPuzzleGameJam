1fcc599079b31e9a74521c600076394315b2f013
ArcaLuiNoe <seby.ro@yahoo.com>
2023-07-27 02:13:57 +0300

    Running Animation w/out decel

--- stick/Assets/Scripts/Player.cs
+++ stick/Assets/Scripts/Player.cs
@@ -1,39 +1,59 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Tilemaps;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    float horizontal;
-    [SerializeField] float acceleration = 1f;
-    [SerializeField] float deceleration = 1f;
-    string horizontalAxis;
-    new Rigidbody2D rigidbody2D;
-
     [SerializeField] float speed;
+
+    Animator animator;
+    Rigidbody2D rb;
+
+    float horizontalMovement;
+    bool facingLeft = true;
 
     void Start()
     {
-        rigidbody2D = GetComponent<Rigidbody2D>();
-        horizontalAxis = "Horizontal";
+        animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
+        Flip();
         ReadHorizontalInput();
-        if (horizontal != 0)
-            MoveHorizontal();
+    }
 
-        void ReadHorizontalInput()
+    void FixedUpdate()
+    {
+        if (horizontalMovement != 0)
         {
-            horizontal = Input.GetAxis(horizontalAxis) * speed;
+            animator.SetFloat("RunningSpeed", 1f);
+            float newHorizontal = Mathf.Lerp(rb.velocity.x, horizontalMovement * speed, Time.fixedDeltaTime * 2f);
+            rb.velocity = new Vector2(newHorizontal, rb.velocity.y);
+        }
+        else
+        {
+            animator.SetFloat("RunningSpeed", -1f);
+            rb.velocity = new Vector2(0, rb.velocity.y);
         }
 
-        void MoveHorizontal()
+    }
+
+    void Flip()
+    {
+        if ((horizontalMovement > 0 && facingLeft) || (horizontalMovement < 0 && !facingLeft))
         {
-            float smoothnessMultiplier = horizontal == 0 ? deceleration : acceleration;
-            float newHorizontal = Mathf.Lerp(rigidbody2D.velocity.x, horizontal * speed, Time.deltaTime * smoothnessMultiplier);
-            rigidbody2D.velocity = new Vector2(newHorizontal, rigidbody2D.velocity.y);
+            facingLeft = !facingLeft;
+            Vector3 localScale = transform.localScale;
+            localScale.x *= -1f;
+            transform.localScale = localScale;
         }
+    }
+
+    void ReadHorizontalInput()
+    {
+        horizontalMovement = Input.GetAxisRaw("Horizontal");
     }
 }
