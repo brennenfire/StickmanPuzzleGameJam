9951cd719ec57ff030e65ab2647433e0dae6a502
brennenfire <barbustefan2002@gmail.com>
2023-07-26 17:06:05 +0300

    simple movement

---
+++ stick/Assets/Scripts/Movement.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    float horizontal; 
+    float acceleration = 1f;
+    float deceleration = 1f;
+    string horizontalAxis;
+    new Rigidbody2D rigidbody2D;
+
+    [SerializeField] float speed;
+
+    void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+        horizontalAxis = "Horizontal";
+    }
+
+    void Update()
+    {
+        ReadHorizontalInput();
+        MoveHorizontal();
+
+        void ReadHorizontalInput()
+        {
+            horizontal = Input.GetAxis(horizontalAxis) * speed;
+        }
+
+        void MoveHorizontal()
+        {
+            float smoothnessMultiplier = horizontal == 0 ? deceleration : acceleration;
+            float newHorizontal = Mathf.Lerp(rigidbody2D.velocity.x, horizontal * speed, Time.deltaTime * smoothnessMultiplier);
+            rigidbody2D.velocity = new Vector2(newHorizontal, rigidbody2D.velocity.y);
+        }
+    }
+}
