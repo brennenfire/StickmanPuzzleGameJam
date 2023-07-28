b3c87018285fc5647c0d0de4a8f39a4ebf5087ca
ArcaLuiNoe <seby.ro@yahoo.com>
2023-07-28 21:02:44 +0300

    Jumping/CoyoteTime/JumpBuffer

---
+++ stick/Assets/Scripts/JumpController.cs
@@ -0,0 +1,103 @@
+using UnityEngine;
+
+public class JumpController : MonoBehaviour
+{
+    [SerializeField] Transform groundCheck;
+    [SerializeField] LayerMask groundLayer;
+    [Header("Jumping Variables")]
+    [SerializeField] float jumpPower;
+    [SerializeField] float fallMultiplier;
+    [SerializeField] float jumpTime;
+    [SerializeField] float jumpMultiplier;
+
+    Rigidbody2D rb;
+    Vector2 vecGravity;
+
+    bool isJumping;
+    float jumpCounter;
+    float jumpBufferCounter;
+    float coyoteTimeCounter;
+
+    void Start()
+    {
+        vecGravity = new Vector2(0, -Physics2D.gravity.y);
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        ReadJumpInput();
+        DoJump();
+        StopJump();
+        CoyoteTime();
+        JumpBuffer();
+
+        if (rb.velocity.y < 0)
+            rb.velocity -= fallMultiplier * Time.deltaTime * vecGravity;
+    }
+
+    private void JumpBuffer()
+    {
+        if (Input.GetButtonDown("Jump"))
+            jumpBufferCounter = 0.2f;
+        else
+            jumpBufferCounter -= Time.deltaTime;
+    }
+
+    private void CoyoteTime()
+    {
+        if (isGrounded())
+            coyoteTimeCounter = .3f;
+        else
+            coyoteTimeCounter -= Time.deltaTime;
+    }
+
+    private void StopJump()
+    {
+        if (Input.GetButtonUp("Jump"))
+        {
+            isJumping = false;
+            jumpCounter = 0;
+
+            if (rb.velocity.y > 0)
+                rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.6f);
+
+            coyoteTimeCounter = 0;
+        }
+    }
+
+    private void DoJump()
+    {
+        if (rb.velocity.y > 0 && isJumping)
+        {
+            jumpCounter += Time.deltaTime;
+            if (jumpCounter > jumpTime)
+                isJumping = false;
+
+            float t = jumpCounter / jumpTime;
+            float currentJumpM = jumpMultiplier;
+
+            if (t > 0.5f)
+                currentJumpM = jumpMultiplier * (1 - t);
+
+            rb.velocity += vecGravity * currentJumpM * Time.deltaTime;
+        }
+    }
+
+    bool isGrounded()
+    {
+        return Physics2D.OverlapCircle(groundCheck.position, 0.14f, groundLayer);
+    }
+
+    void ReadJumpInput()
+    {
+        if (jumpBufferCounter > 0 && coyoteTimeCounter > 0)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpPower);
+            isJumping = true;
+            jumpCounter = 0;
+
+            jumpBufferCounter = 0;
+        }
+    }
+}
