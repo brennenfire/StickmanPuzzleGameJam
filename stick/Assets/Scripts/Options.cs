473ea1ec1e57e6d9320c0894066c6a4d1328cd27
brennenfire <barbustefan2002@gmail.com>
2023-07-29 20:37:59 +0300

    first implementation of options menu

---
+++ stick/Assets/Scripts/Options.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Options : MonoBehaviour
+{
+    public void Quit()
+    {
+        Debug.Log("quit app");
+        Application.Quit();
+    }
+}
