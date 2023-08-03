2d5ef5715bc30bf3e23ec963fd185b5b4380ed83
brennenfire <barbustefan2002@gmail.com>
2023-08-04 01:48:12 +0300

    small script change

--- stick/Assets/Scripts/PuzzleStuff/PositionReset.cs
+++ stick/Assets/Scripts/PuzzleStuff/PositionReset.cs
@@ -7,10 +7,18 @@ public class PositionReset : MonoBehaviour
     [SerializeField] int counter;
     [SerializeField] bool physics;
 
+    new Collider2D collider;
+
+    void Awake()
+    {
+        collider = GetComponent<Collider2D>();    
+    }
+
     void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.GetComponent<Player>() != null)
         {
+            collider.enabled = false;
             LineCreator.Instance.lineCounter = counter;
             LineCreator.Instance.usePhysics = physics;
             LineCreator.Instance.initialLineCounter = counter;
