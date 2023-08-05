52f0fd8e9fdf4eba260bed06d829e3743dedfaf4
brennenfire <barbustefan2002@gmail.com>
2023-08-05 19:05:36 +0300

    small changes to scripts and map

--- stick/Assets/Scripts/Options.cs
+++ stick/Assets/Scripts/Options.cs
@@ -1,9 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class Options : MonoBehaviour
 {
+    [SerializeField] TMP_Text showInk;
+    int fakeInk;
+
+    void Update()
+    {
+        fakeInk = (int)LineCreator.Instance.ink * 10;
+        showInk.SetText("ink: " + fakeInk.ToString());
+    }
+
     public void Quit()
     {
         Debug.Log("quit app");
